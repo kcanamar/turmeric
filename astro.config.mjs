e56c082178771154c19643f07a477343c5badbf5
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-08-23 18:52:26 -0700

    mounts vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    // This will inject Vercel's tracking scripts into all pages
+    analytics: true
+  })
+});
